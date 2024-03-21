0182f3f979c69db7b303ceb75130902e6811a633
LuanMiranda77 <luanprof30@gmail.com>
2024-03-20 22:52:37 -0300

    ajuste

--- src/App.tsx
+++ src/App.tsx
@@ -41,7 +41,7 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonTabs>
-        <IonRouterOutlet>
+        <IonRouterOutlet onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
           <Route exact path="/tab1">
             <Tab1 />
           </Route>
